23733d1655e7d3a92dc1874467999b0a61f767fd
Yaniv Aharon <ayaniv@gmail.com>
2020-05-01 02:01:15 +0300

    npm run build

---
+++ build/precache-manifest.382a567abb834a91c306f3efa96dd45f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d97acac05fe9560363b014e21d4e6cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df2d5dd820590d557d06",
+    "url": "/static/css/main.93790eba.chunk.css"
+  },
+  {
+    "revision": "ac6eaa42a1093b455a21",
+    "url": "/static/js/2.015a3c2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.015a3c2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df2d5dd820590d557d06",
+    "url": "/static/js/main.ce4ef398.chunk.js"
+  },
+  {
+    "revision": "aad59be699d47996e54c",
+    "url": "/static/js/runtime-main.acec2b9d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
